20659f9fc32336e1913817e71ff3e9bc53731c54
19-Ag <goodstudent1919@gmail.com>
2024-06-27 17:50:24 +0530

    Update hangman_basic.py

--- hangman_basic.py
+++ hangman_basic.py
@@ -1,14 +1,84 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 display = ["_"] * len(chosen_word)
+lives = 6
 
-while "_" in display:
+while "_" in display and lives > 0:
     guess = input("Enter a letter: ").lower()
+    if len(guess) != 1 or not guess.isalpha():
+        print("Please enter a single letter.")
+        continue
+    if guess in display:
+        print("You've already guessed this letter.")
+        continue
     for i in range(len(chosen_word)):
         if guess == chosen_word[i]:
             display[i] = guess
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"Incorrect! You have {lives} lives left.")
+        print(stages[6 - lives])
     print(" ".join(display))
 
-print("Congratulations, you won!")
+if "_" not in display:
+    print("Congratulations, you won!")
+else:
+    print("Game over! The word was " + chosen_word)
